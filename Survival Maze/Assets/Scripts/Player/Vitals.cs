448b58d8b6eb68fcfbde82abf0e1a5f5dbef9287
jorisaikali <jori.saikali@hotmail.com>
2018-01-27 18:08:48 -0500

    Commented up the player vital scripts

--- Survival Maze/Assets/Scripts/Player/Vitals.cs
+++ Survival Maze/Assets/Scripts/Player/Vitals.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// This script allows the inspector in Unity to see Vital as an object
+// Click player then in "Player Vitals (Script)" component, you'll see "Vitals"
+// that is a List where each element of the list is the Vital object 
+
 [System.Serializable]
 public class Vital {
 
