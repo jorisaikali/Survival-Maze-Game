0acacd94093eb5c23759286276282f6e5c289e81
jorisaikali <jori.saikali@hotmail.com>
2018-02-02 00:02:34 -0500

    Added Inventory System functionality
    Okay so this is a very big commit which I should not have done and apologize for. But here it goes,
    
    Major Changes:
    	- Added Inventory functionality
    		- InventoryController script
    			- Controls all core inventory functionality and rules
    		- InventorySlot
    			- Controls all inventory slots
    		- InventoryUI
    			- Controls all inventory UI related functionality
    
    	- Added Equipped functionality
    		- EquipmentController
    			- Controls all core equipped functionality and rules
    		- EquipmentSlot
    			- Controls all equipped slots
    		- EquipmentUI
    			- Controls all equipped UI related functionality
    
    	- Added Item blueprints
    		- Item
    			- Superclass all item objects inherit from
    		- Equipment
    			- Blueprint for equipment items (inherits from Item)
    		- Consumable
    			- Blueprint for consumable items (inherits from Item)
    		- Resource
    			- Blueprint for resource items (inherits from Item)
    
    	- Added a helper class to remove duplicates
    		- DistinctItemComparer
    			- This class compares items and detects if the inventory has the same Resource item already
    
    Minor Changes:
    	- Added more functionality to the PlayerVitals script
    		- New functions to regenerate vitals
    	- Added more prefabs
    	- Added temporary sprites
    	- Added Test Items for testing the inventory

---
+++ Survival Maze/Assets/Scripts/Inventory/InventoryUI.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+// This script is on the Canvas game object
+// Purpose: This script deals with all UI related functionality for the inventory. This includes opening the inventory when the player presses the tab key and updating the UI
+//          when something changes in the inventory.
+public class InventoryUI : MonoBehaviour {
+
+    // --------- Public variables --------- //
+    public Transform itemsParent;
+    public GameObject inventoryUI;
+    // ------------------------------------ //
+
+    // --------- Private variables --------- //
+    private InventoryController inventory;
+    private InventorySlot[] slots;
+    private List<Item> items = new List<Item>();
+    // ------------------------------------- //
+
+    void Start () {
+        inventory = InventoryController.instance; // getting the singleton instance of InventoryController
+        inventory.onItemChangedCallback += UpdateUI; // subscribing UpdateUI() to onItemChangedCallback event
+        inventory.onResourceChangedCallback += ConvertAndUpdateResourceUI; // subscribing ConvertAndUpdateResourceUI to onResourceChangedCallback event
+
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>(); // getting a reference to all slots
+    }
+	
+	void Update () {
+		if (Input.GetButtonDown("Inventory")) // if tab is pressed
+        {
+            inventoryUI.SetActive(!inventoryUI.activeSelf); // if inventory open->close, if closed->open
+
+            if (Cursor.lockState == CursorLockMode.Locked) // if cursor is locked, unlock it, if it's unlocked, lock it
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            
+            Cursor.visible = !Cursor.visible; // set the cursors visibility to opposite of what it currently is
+        }
+	}
+
+    // ---------- Convertting Item -> Resource ---------- //
+    private void ConvertAndUpdateResourceUI(Item item)
+    {
+        UpdateResourceUI((Resource)item); // Convert Item to Resource and pass it to UpdateResourceUI
+    }
+    // -------------------------------------------------- //
+
+    // ---------- Updating the resource UI ----------- //
+    private void UpdateResourceUI(Resource item)
+    {
+        for (int i = 0; i < slots.Length; i++) // iterate through all slots
+        {
+            if (slots[i].GetItem().name == item.name) // if slots[i] items name equals the new items name
+            {
+                slots[i].stackCount++; // increase the count because if in this if statement it means the Resource item is already in the players inventory
+                item.stackAmount++; // increase the stackAmount on the actual item too because if the item is dropped and picked up again it needs to have the same stackAmount
+
+                inventory.items = inventory.items.Distinct(new DistinctItemComparer()).ToList(); // Removes duplicate resources in the inventory
+                UpdateUI();
+
+                return;
+            }
+        }
+    }
+    // ----------------------------------------------- //
+
+    // ----------------------- Updating the inventory UI ----------------------- //
+    // This happens whenever the inventory changes somehow
+    private void UpdateUI()
+    {
+        inventory.items = inventory.items.Distinct(new DistinctItemComparer()).ToList(); // Removes duplicate resources in the inventory
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory.items.Count) // if i < # of items in the inventory list (found in Inventory script)
+            {
+                slots[i].AddItem(inventory.items[i]); // calling AddItem from InventorySlot, adding the item to the slot
+            }
+            else
+            {
+                slots[i].ClearSlot(); // Clear the rest of the slots
+            }
+        }
+    }
+    // ------------------------------------------------------------------------- //
+}
---
+++ Survival Maze/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+// This creates a new object that can be created in the unity editor. We can use this as a blueprint for items in our game.
+// To create an item, right click in the Project window -> Create -> Inventory -> Item. This now creates an Item asset.
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    // ----------- Public variables ------------ //
+    new public string name = "New Item";
+    public Sprite icon = null;
+    public bool isDefaultItem = false;
+    public ItemType itemType;
+    public GameObject mesh;
+    // ----------------------------------------- //
+
+    // ----------- Virtual functions ------------ //
+    // virtual functions are functions that can be overrided in classes that inherit the Item class
+    public virtual void Use() { }  
+    public virtual void Use(GameObject player) { }
+    // ------------------------------------------ //
+}
+
+public enum ItemType { Consumable, Equipment, Resource }
