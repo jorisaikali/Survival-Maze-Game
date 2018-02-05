a54e6f6a20d19631cf524dc6dcc37a3e327b8d0a
Imran <imran@loveairbear.com>
2018-02-05 01:05:01 -0500

    fixed .gitignore

---
+++ Survival Maze/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+	// Aggregate all components for updating
+	PlayerCombat playerCombat;
+	PlayerMovement playerMovement;
+	PlayerVitals playerVitals;
+
+	void Start () {
+		playerCombat = GetComponent<PlayerCombat>();
+		playerMovement = GetComponent<PlayerMovement>();
+		playerVitals = GetComponent<PlayerVitals>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isLocalPlayer) {
+			return;
+		}
+		playerCombat.UpdateMe();
+		playerMovement.UpdateMe();
+		playerVitals.UpdateMe();
+	}
+}
