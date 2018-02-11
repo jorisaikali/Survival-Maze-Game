571717818e0b1922b73b37fb2270731f76ac0d0c
Imran <imran@loveairbear.com>
2018-02-11 14:31:14 -0500

    stashing minor changes to networking

--- Survival Maze/Assets/Scripts/Player/Weapon.cs
+++ Survival Maze/Assets/Scripts/Player/Weapon.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Weapon : MonoBehaviour {
+public class Weapon : NetworkBehaviour {
 
 	void OnTriggerEnter(Collider other){
 
-		if(other.gameObject.layer == 8) { //if collide with anything in entities layer
+		if(other.gameObject.layer == 8 && other.gameObject.tag != "LocalPlayer") { //if collide with anything in entities layer
 			//all entities must have vitals
-			other.gameObject.SendMessage("UpdateHealth",-1f); //this calls the entity's method UpdateHealth
+			 //this calls the entity's method UpdateHealth
 			//Debug.Log("weapon collision");
 		}
+	}
+
+	void ApplyDamage(GameObject target) {
+		Debug.Log("hit other player");
+		target.SendMessage("UpdateHealth",-1f);
 	}
 
 	// ------------Weapon attack animation and return animation -------- //
